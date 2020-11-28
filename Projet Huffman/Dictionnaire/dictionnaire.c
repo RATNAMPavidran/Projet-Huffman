ee473dfb698bb1c2a1a17cb942d4dd3f27e701b4
Pavidran RATNAM <pavidran.ratnam@efrei.net>
2020-11-28 16:01:01 +0100

    ajout du code des fonctions

---
+++ Projet Huffman/Dictionnaire/dictionnaire.c
@@ -0,0 +1,61 @@
+void creer_code(Noeud* Arbre, char* code, FILE* dico) // Création du dictionnaire à partir d'un arbre donné
+{
+    char copie[50];
+
+    if (Arbre != NULL)
+    {
+        if ((Arbre->droite == NULL) && (Arbre->gauche == NULL))  //feuille
+        {
+            //printf("%s est le code\n",code); 
+            fprintf(dico, "%c : %s\n", Arbre->lettre, code); // écrire dans le fichier dico le chemin
+        }
+
+        else
+        {
+            strcpy(copie, code);          // copie = code
+            strcat(code, "0");           //concaténer les contenus de deux chaînes de caractères
+            creer_code(Arbre->gauche, code, dico);         //On va à gauche = 0
+            strcat(copie, "1");                          // On va à droite = 1
+            creer_code(Arbre->droite, copie, dico);
+        }
+    }
+}
+
+
+void dico_binaire(FILE* dico, char caractere, char* trad)// traduire un caractere en binaire à partir du dico
+{
+    char caractereActuel;
+    int i = 0;
+    if (dico != NULL)
+    {
+        rewind(dico);  //replace le curseur au début du fichier
+
+        //printf("%c est caractere\n", caractere);
+        while (caractere != caractereActuel)      // on recherche le caractère dans le dico
+        {
+            caractereActuel = fgetc(dico);
+        }
+
+        while ((caractereActuel != '0') && (caractereActuel != '1'))
+        {
+            caractereActuel = fgetc(dico);
+        }
+
+        do
+        {
+
+            //  printf("%c est caractere %d \n", caractereActuel, i);
+             // if (caractereActuel!='\n')
+             // {
+            trad[i] = caractereActuel;     // on place le chiffre dans le tableau
+            caractereActuel = fgetc(dico);
+            //}
+            i++;
+
+        } while (caractereActuel != '\n');
+
+        trad[i] = '\0';
+
+        // printf("%s est le code bin", trad);
+    }
+}
--- Projet Huffman/Dictionnaire/dictionnaire.h
+++ Projet Huffman/Dictionnaire/dictionnaire.h
@@ -1,1 +1,8 @@
-#pragma once
+#ifndef DICTIONNAIRE_H_INCLUDED
+#define DICTIONNAIRE_H_INCLUDED
+
+#include "arbre.h"
+
+void creer_code(Noeud* Arbre, char* code, FILE* dico);
+
+void dico_binaire(FILE* dico, char caractere, char* trad);
