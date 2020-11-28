eb7b6e0a7d099d2269edfc89f9d12c192176847b
Pavidran RATNAM <pavidran.ratnam@efrei.net>
2020-11-28 15:16:14 +0100

    Ajout des signatures des diffférentes fonctions

---
+++ Projet Huffman/Arbre/arbre.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "arbre.h"
+
+
+void trad_fichier_ascii(FILE* f) {
+}
+
+char* binaire(char a) {
+	return octet;
+}
+
+int nombre_caractere_fich_txt(FILE* f) {
+	return numbre;
+}
+
+Element* compter_occurrence(FILE* f) {
+	return lsc;
+}
+
+void afficherListe(Element* liste) {
+}
+
+void creer_liste(L_Noeud** l_nd, Element* l) {
+}
+
+void afficherListe_Noeud(L_Noeud* liste) {
+}
+
+void Add_Liste_Noeud(L_Noeud** l, Noeud* n) {
+}
+
+void trie_liste(Element** liste) {
+}
+
+void Arbre_Huff(Element* l) {
+}
+
+void print_tree(Noeud* tree) {
+}
+
+void creer_code(Noeud* Arbre, char* code, FILE* dico) {
+}
