777887516a5aeda23d2494c90d90c2c8d76e70ae
Pavidran RATNAM <pavidran.ratnam@efrei.net>
2020-11-28 17:17:26 +0100

    ajout des commentaires doxygen

--- Projet Huffman/Dictionnaire/dictionnaire.h
+++ Projet Huffman/Dictionnaire/dictionnaire.h
@@ -1,1 +1,17 @@
-#pragma once
+/*****************************************************************//**
+ * \file   dictionnaire.H
+ * \brief  Header of the library allowing the management of dictionnaire.
+ *
+ * \author  Mélina, Arnaud, Pavidran, Olivier, Tommy
+ * \date   November 2020
+ *********************************************************************/
+
+
+#ifndef DICTIONNAIRE_H_INCLUDED
+#define DICTIONNAIRE_H_INCLUDED
+
+#include "arbre.h"
+
+void creer_code(Noeud* Arbre, char* code, FILE* dico);
+
+void dico_binaire(FILE* dico, char caractere, char* trad);
