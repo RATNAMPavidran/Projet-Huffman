bc56f3d76285aff7e22a485820b6f6ca3c413c0c
MelWng <73227471+MelWng@users.noreply.github.com>
2020-12-07 15:12:25 +0100

    Update arbre.h

--- Projet Huffman/Arbre/arbre.h
+++ Projet Huffman/Arbre/arbre.h
@@ -1,6 +1,40 @@
 #ifndef ARBRE_H_INCLUDED
 #define ARBRE_H_INCLUDED
 
+typedef struct Element Element;
+struct Element
+{
+    char lettre;
+    int nombre;
+    Element *suivant;
+};
+
+
+typedef struct Noeud Noeud;
+struct Noeud
+{
+    int poids;
+    char lettre;
+
+    char code[30];
+    Noeud *droite;
+    Noeud *gauche;
+};
+
+
+typedef struct L_Noeud L_Noeud;
+struct L_Noeud
+{
+    Noeud* n;
+    L_Noeud *suivant;
+};
+
+typedef struct Queue Queue;
+struct Queue
+{
+    L_Noeud* liste_noeud;
+};
+
 Noeud **creer_tab_occur(FILE* f,int *taille);
 L_Noeud * Tab_to_L_Noeud(Noeud ** tab , int nbre_chara_diff );
 Noeud* fusionner_deux_noeuds(Noeud*n1,Noeud*n2);
