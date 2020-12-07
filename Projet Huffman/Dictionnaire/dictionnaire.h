14fdecf71e6dfdfc5d1bd5c728e20f219b87dbee
MelWng <73227471+MelWng@users.noreply.github.com>
2020-12-07 15:21:15 +0100

    Update dictionnaire.h

--- Projet Huffman/Dictionnaire/dictionnaire.h
+++ Projet Huffman/Dictionnaire/dictionnaire.h
@@ -1,1 +1,15 @@
-#pragma once
+#ifndef DICTIONNAIRE_H_INCLUDED
+#define DICTIONNAIRE_H_INCLUDED
+
+#include "arbre.h"
+
+void creer_code_v2(Noeud *Arbre, char* code,Noeud**tab){
+
+}
+
+int chercher_lettre_avl(Noeud *avl,char* code, char* lettre){
+  return 0;
+}
+
+
+#endif // DICTIONNAIRE_H_INCLUDED
