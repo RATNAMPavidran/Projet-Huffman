e92a9748bbd3eb58f0a43b229db2b5c27b7ab020
MelWng <73227471+MelWng@users.noreply.github.com>
2020-12-07 15:03:10 +0100

    Update arbre.c

---
+++ Projet Huffman/Arbre/arbre.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "arbre.h"
+
+
+Noeud **creer_tab_occur(FILE* f,int *taille){
+             return(tab);
+}
+
+L_Noeud * Tab_to_L_Noeud(Noeud ** tab , int nbre_chara_diff ){
+             return (list);
+}
+
+Noeud* fusionner_deux_noeuds(Noeud*n1,Noeud*n2){
+            return (nouv_noeud);
+} 
+
+
+Noeud ** creer_2_premiers_noeud (Noeud** tab,int* taille,FILE* f){
+            return (tab);
+}
+void quick_sort(Noeud*** tab, int debut, int fin){
+            quick_sort(tab, debut,j-1);
+            quick_sort(tab,j+1, fin);
+}
+
+void Afficher_tab(int taille, Noeud**tab){
+            
+}
+
+Noeud* Arb_Huff_queue(Noeud **tab,int taille){
+    return (q2->liste_noeud->n);
+}
