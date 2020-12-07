5309e10815076fa4e5c87555aa6f061a3d5c02a0
MelWng <73227471+MelWng@users.noreply.github.com>
2020-12-07 15:18:00 +0100

    Update dictionnaire.c

---
+++ Projet Huffman/Dictionnaire/dictionnaire.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dictionnaire.h"
+
+void creer_code_v2(Noeud *Arbre, char* code,Noeud**tab){
+
+}
+
+int chercher_lettre_avl(Noeud *avl,char* code, char* lettre){
+  return 0;
+}
