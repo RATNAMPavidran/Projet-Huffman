139c261acc6ebbe40d7c40d79f5076b499f4a092
MelWng <73227471+MelWng@users.noreply.github.com>
2020-12-12 09:27:41 +0100

    Update encodage.h

--- Projet Huffman/Encodage/encodage.h
+++ Projet Huffman/Encodage/encodage.h
@@ -1,8 +1,9 @@
+
 /*****************************************************************//**
  * \file   encodage.h
  * \brief  Header of the library allowing the management of encodage.
  *
- * \author  Mélina, Arnaud, Pavidran, Olivier, Tommy
+ * \author  MÃ©lina, Arnaud, Pavidran, Olivier, Tommy
  * \date   November 2020
  *********************************************************************/
 ifndef ENCODAGE_H_INCLUDED
